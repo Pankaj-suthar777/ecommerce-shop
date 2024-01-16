e326d843297b99716456b8e20cface81b68b3975
pankaj suthar <pankajss0070@gmail.com>
2024-01-16 14:18:40 +0530

    done with some styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <div>Welcome</div>;
+  return (
+    <>
+      <Header></Header>
+      <main className="py-3">
+        <Container>
+          <Outlet></Outlet>
+        </Container>
+      </main>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
